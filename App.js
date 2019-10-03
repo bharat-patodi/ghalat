c84ab0dcd58216cd1fb3c987f0a4d668b2fbeb15
bharat-patodi <bharatpatodi@gmail.com>
2019-10-03 12:32:32 +0530

    Preparing layout

--- App.js
+++ App.js
@@ -8,42 +8,142 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
+  Button,
+  StatusBar
 } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack' // first install the stack
+import { createStackNavigator } from 'react-navigation-stack';
 import Header from './src/components/Header';
 
-const App: () => React$Node = () => {
+// Home Screen
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar barStyle = "dark-content"/>
+        <Header />
+        <View>
+          <Text>This is the start.</Text>
+          <Text>Screen 01: Splash screen</Text>
+          <Text>Screen 02: Home screen</Text>
+          <Button title= "Go to Home" onPress = {
+            () => this.props.navigation.navigate('Home')
+          } />
+          <Text>Screen 03: Game screen</Text>
+          <Button title= "Go to Game" onPress = {
+            () => this.props.navigation.navigate('Game')
+          } />
+          <Text>Screen 03-01: Question Screen</Text>
+          <Button title= "Go to Question" onPress = {
+            () => this.props.navigation.navigate('Question')
+          } />
+          <Text>Screen 03-02: Final Results screen</Text>
+          <Button title= "Go to Results" onPress = {
+            () => this.props.navigation.navigate('Results')
+          } />
+          <Text>Screen 04: My steps screen</Text>
+          <Text>This is the end.</Text>
+          <Button title= "Go to the end" onPress = {
+            () => this.props.navigation.navigate('End')
+          } />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+// Game Screen
+
+class GameScreen extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>This is where we show the Game.</Text>
+      </View>
+    )
+  }
+}
+
+// Question Screen
+
+class QuestionScreen extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>This is where we show the Questions.</Text>
+      </View>
+    )
+  }
+}
+
+// Results Screen
+
+class ResultsScreen extends React.Component {
+  render() {
+    return(
+      <View>
+        <Text>Results are in.</Text>
+      </View>
+    )
+  }
+}
+
+// End Screen
+
+class EndScreen extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>Sab Khatm.</Text>
+      </View>
+    )
+  }
+}
+
+
+
+// Navigation
+const MyAppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Game: {
+    screen: GameScreen,
+  },
+  Question: {
+    screen: QuestionScreen,
+  },
+  Results: {
+    screen: ResultsScreen,
+  },
+  End: {
+    screen: EndScreen,
+  }
+
+},
+{
+  initialRouteName: 'Home',
+}
+
+);
+
+const MyAppContainer = createAppContainer(MyAppNavigator);
+
+export default class App extends React.Component {
+  render () {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView>
-        <Header />
-          <View>
-            <Text>This is the start.</Text>
-            <Text>Screen 01: Splash screen</Text>
-            <Text>Screen 02: Menu screen</Text>
-            <Text>Screen 03: Game screen</Text>
-            <Text>Screen 03-01: Question Screen</Text>
-            <Text>Screen 03-02: Final Results screen</Text>
-            <Text>Screen 04: My steps screen</Text>
-            <Text>This is the end.</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <MyAppContainer />
     </>
-  );
+  );}
 };
+
 
 const styles = StyleSheet.create({
 
 });
-
-export default App;
