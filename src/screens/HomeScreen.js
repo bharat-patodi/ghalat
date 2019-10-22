59a79d7b8587d205e2b9a0fce3c7f02b4676446f
bharat-patodi <bharatpatodi@gmail.com>
2019-10-22 15:45:22 +0530

    WIP

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,3 +1,131 @@
+// Import statements
+import React from 'react';
+import { View, Button, Text } from 'react-native';
+import allStyles from '../styles/styles';
+
+// Create the component
+class HomeScreen extends React.Component {
+    render () {
+        return (
+            <View style={[allStyles.HomeScreen]}>
+                <View>
+                    <Text style={{ fontSize: 20 }}>Welcome to the mind-bending game of Ghalat ... where the wrong answer is the right one. It is refreshing for the mind to try activities like these and I encourage everyone to play it for a while.</Text>
+                    <Button
+                        title="Begin Game"
+                        onPress={
+                            () => this.props.navigation.navigate('Question')
+                        }
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+// Export the component
+export default HomeScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // import statements
 import React from 'react';
 import { Text, View, Button, TextInput, TouchableHighlight } from 'react-native';
@@ -85,3 +213,5 @@ export default class HomeScreen extends React.Component {
         )
     }
 }
+
+*/
